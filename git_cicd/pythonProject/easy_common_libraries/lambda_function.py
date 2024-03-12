016e755651a81f363d5c11f5473ff4b7d91c82f6
Vijaykumar <vijaykumarkhose@gmail.com>
2024-03-12 10:13:14 +0000

    first cicd lambda

---
+++ git_cicd/pythonProject/easy_common_libraries/lambda_function.py
@@ -0,0 +1,12 @@
+import json
+from  math_functions import *
+
+
+# def lambda_handler(event, context):
+def lambda_handler():
+    num = 100
+    print("Square of the number is :-- ", getSquare(num))
+    return getSquare(num)
+
+# if __name__ == "__main__":
+#     lambda_handler()
